e49a6b23adaf7ed8a52c7f560e91e99d93fd7cf5
Tutilabs <https://github.com/Tutilabs>
2021-03-08 08:19:38 -0400

    pwa ok

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 import Cabecalho from '../components/Cabecalho';
+import  Head  from 'next/head';
+
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <link rel="manifest" href="/manifest/manifest.webmanifest"/>
+      </Head>
     <Cabecalho/>
     </div>
   )
